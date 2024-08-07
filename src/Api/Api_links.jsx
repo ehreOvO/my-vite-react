52df69075faf1345fa4aa315b77bebc9251dea07
lan hao <66598058+ehreOvO@users.noreply.github.com>
2024-08-07 11:28:16 +0800

    Update Api_links.jsx

--- src/Api/Api_links.jsx
+++ src/Api/Api_links.jsx
@@ -1,63 +1,51 @@
 const Api_links = {
-  Account: {
-    Players: "https://api.worldofwarships.asia/wows/account/list/",
+  URL_ADDRESS: {
+    asia: "https://api.worldofwarships.asia/",
+    eu: "https://api.worldofwarships.eu/",
+    na: "https://api.worldofwarships.com/",
+  },
 
-    Player_personal_data: "https://api.worldofwarships.asia/wows/account/info/",
-    Players_achievements:
-      "https://api.worldofwarships.asia/wows/account/achievements/",
-    Player_statistics_by_date:
-      "https://api.worldofwarships.asia/wows/account/statsbydate/",
-  },
-  Encyclopedia: {
-    Information_about_encyclopedia:
-      "https://api.worldofwarships.asia/wows/encyclopedia/info/",
-    Warships: "https://api.worldofwarships.asia/wows/encyclopedia/ships/",
-    Achievements:
-      "https://api.worldofwarships.asia/wows/encyclopedia/achievements/",
-    Ship_parameters:
-      "https://api.worldofwarships.asia/wows/encyclopedia/shipprofile/",
-    Modules: "https://api.worldofwarships.asia/wows/encyclopedia/modules/",
-    Service_Record_levels_information:
-      "https://api.worldofwarships.asia/wows/encyclopedia/accountlevels/",
-    Commanders: "https://api.worldofwarships.asia/wows/encyclopedia/crews/",
-    Commander_Skills:
-      "https://api.worldofwarships.asia/wows/encyclopedia/crewskills/",
-    Commanders_Ranks:
-      "https://api.worldofwarships.asia/wows/encyclopedia/crewranks/",
-    Battle_types:
-      "https://api.worldofwarships.asia/wows/encyclopedia/battletypes/",
-    Consumables:
-      "https://api.worldofwarships.asia/wows/encyclopedia/consumables/",
-    Collections:
-      "https://api.worldofwarships.asia/wows/encyclopedia/collections/",
-    Collection_items:
-      "https://api.worldofwarships.asia/wows/encyclopedia/collectioncards/",
-    Maps: "https://api.worldofwarships.asia/wows/encyclopedia/battlearenas/",
-  },
-  Warships: {
-    Statistics_of_players_ships:
-      "https://api.worldofwarships.asia/wows/ships/stats/",
-  },
-  Seasons: {
-    Ranked_Battles_seasons:
-      "https://api.worldofwarships.asia/wows/seasons/info/",
-    Ships_statistics_in_Ranked_Battles:
-      "https://api.worldofwarships.asia/wows/seasons/shipstats/",
-    Players_statistics_in_Ranked_Battles:
-      "https://api.worldofwarships.asia/wows/seasons/accountinfo/",
-  },
-  Clans: {
-    Clans: "https://api.worldofwarships.asia/wows/clans/list/",
-    Clan_details: "https://api.worldofwarships.asia/wows/clans/info/",
-    Player_clan_data:
-      "https://api.worldofwarships.asia/wows/clans/accountinfo/",
-    Clan_glossary: "https://api.worldofwarships.asia/wows/clans/glossary/",
-    Clan_Battle_Seasons: "https://api.worldofwarships.asia/wows/clans/season/",
-    Clan_battles_season_stats:
-      "https://api.worldofwarships.asia/wows/clans/seasonstats/",
+  PATH_ADDRESS: {
+    Account: {
+      Players: "/wows/account/list/",
+
+      Player_personal_data: "/wows/account/info/",
+      Players_achievements: "/wows/account/achievements/",
+      Player_statistics_by_date: "/wows/account/statsbydate/",
+    },
+    Encyclopedia: {
+      Information_about_encyclopedia: "/wows/encyclopedia/info/",
+      Warships: "/wows/encyclopedia/ships/",
+      Achievements: "/wows/encyclopedia/achievements/",
+      Ship_parameters: "/wows/encyclopedia/shipprofile/",
+      Modules: "/wows/encyclopedia/modules/",
+      Service_Record_levels_information: "/wows/encyclopedia/accountlevels/",
+      Commanders: "/wows/encyclopedia/crews/",
+      Commander_Skills: "/wows/encyclopedia/crewskills/",
+      Commanders_Ranks: "/wows/encyclopedia/crewranks/",
+      Battle_types: "/wows/encyclopedia/battletypes/",
+      Consumables: "/wows/encyclopedia/consumables/",
+      Collections: "/wows/encyclopedia/collections/",
+      Collection_items: "/wows/encyclopedia/collectioncards/",
+      Maps: "/wows/encyclopedia/battlearenas/",
+    },
+    Warships: {
+      Statistics_of_players_ships: "/wows/ships/stats/",
+    },
+    Seasons: {
+      Ranked_Battles_seasons: "/wows/seasons/info/",
+      Ships_statistics_in_Ranked_Battles: "/wows/seasons/shipstats/",
+      Players_statistics_in_Ranked_Battles: "/wows/seasons/accountinfo/",
+    },
+    Clans: {
+      Clans: "/wows/clans/list/",
+      Clan_details: "/wows/clans/info/",
+      Player_clan_data: "/wows/clans/accountinfo/",
+      Clan_glossary: "/wows/clans/glossary/",
+      Clan_Battle_Seasons: "/wows/clans/season/",
+      Clan_battles_season_stats: "/wows/clans/seasonstats/",
+    },
   },
 };
-
-// console.log(Api_links);
 
 export default Api_links;
